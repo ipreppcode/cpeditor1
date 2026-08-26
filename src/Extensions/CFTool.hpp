15e9733603626e5ce16a129462ae6754791de387
ipreppcode <132910855+ipreppcode@users.noreply.github.com>
2026-08-26 15:46:22 +0530

    Simplify CFTool for manual browser submission

--- src/Extensions/CFTool.hpp
+++ src/Extensions/CFTool.hpp
@@ -1,18 +1,8 @@
 /*
  * Copyright (C) 2019-2021 Ashar Khan <ashar786khan@gmail.com>
+ * Modified for browser-based manual submission.
  *
  * This file is part of CP Editor.
- *
- * CP Editor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * I will not be responsible if CP Editor behaves in unexpected way and
- * causes your ratings to go down and or lose any important contest.
- *
- * Believe Software is "Software" and it isn't immune to bugs.
- *
  */
 
 #ifndef CFTOOL_HPP
@@ -45,13 +35,12 @@ class CFTool : public QObject
     void onFinished(int exitCode, QProcess::ExitStatus);
 
   private:
-    QString problemContestId, problemCode, lastStatus;
+    QString problemContestId, problemCode;
     QProcess *CFToolProcess = nullptr;
     MessageLogger *log;
     QString CFToolPath;
 
     void showToastMessage(const QString &message);
-    QString getCFToolVersion() const;
 };
 } // namespace Extensions
 
